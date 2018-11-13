9b38b0118be5408a762160f87d61949746c5ffda
Jackson Okuhn <jackson@wave.com>
2018-11-13 02:15:06 +0000

    added channel arg

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 SLACK_OUTGOING_WEBHOOK_TOKEN = os.environ.get('SLACK_OUTGOING_WEBHOOK_TOKEN', None)
-SLACK_INCOMING_WEBHOOK_URL = os.environ['SLACK_INCOMING_WEBHOOK_URL']
+ANON_URL = os.environ['ANON_URL']
+KINK_URL = os.environ['KINK_URL']
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     token = request.form.get('token')
-    print(token)
-    print(SLACK_OUTGOING_WEBHOOK_TOKEN)
     if not SLACK_OUTGOING_WEBHOOK_TOKEN or token == SLACK_OUTGOING_WEBHOOK_TOKEN:
+        text = request.form.get('text')
+        first, _, resp_text = text.partition(" ")
+        if first == '#kink':
+            incoming_url = KINK_URL
+        else:
+            incoming_url = ANON_URL
+            resp_text = text
         params = {
                 'username': 'anonbot',
                 'icon_emoji': ':ghost:',
-                'text': request.form.get('text'),
+                'text': resp_text,
                 'channel': '#%s' % request.form.get('channel_name')
         }
         try:
-            response = requests.post(SLACK_INCOMING_WEBHOOK_URL, data=json.dumps(params))
+            response = requests.post(incoming_url, data=json.dumps(params))
         except Exception as e:
             return 'Posting to Slack failed: %s' % str(e)
     return ''
